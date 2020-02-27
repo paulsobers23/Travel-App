984dead3e4218186e8a7b1ffffb2fa7f23cf64f6
EC2 Default User <ec2-user@ip-172-31-40-240.us-east-2.compute.internal>
2020-02-27 01:00:16 +0000

    started making request and install unirest library for handling request easier

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
+const url2 = 'https://randomuser.me/api'
+const rapidApiUrl = 'https://cors-anywhere.herokuapp.com//https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm'
+
+const requestMethod = (method, url , data) => {
+	return fetch(url, {
+		method: method,
+		body: JSON.stringify(data),
+		headers: {
+		  "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		  "x-rapidapi-key": "e7dc0e5c66msha7c62a4525b23dfp1beafdjsnd67679c4edc0"
+		}
+		})
+		  .then((response)=>{
+		    console.log(response)
+		  })
+		  .catch( err => {
+		    console.log(err)
+		  })
+};
+
 const getData = async() =>{
+  const data = await requestMethod('GET',rapidApiUrl)
+  // const response = await data.json
   
-  const data = await fetch("https://cors-anywhere.herokuapp.com//https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01?inboundpartialdate=2019-12-01", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-		"x-rapidapi-key": "e7dc0e5c66msha7c62a4525b23dfp1beafdjsnd67679c4edc0"
-	}
-})
-  .catch(err => {
-	console.log(err);
-});
-  console.log(data)
   return data
-  
 }
-
 getData()
-
-// fetch("https://cors-anywhere.herokuapp.com//https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01?inboundpartialdate=2019-12-01", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "e7dc0e5c66msha7c62a4525b23dfp1beafdjsnd67679c4edc0"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-//   .catch(err => {
-// 	console.log(err);
-// });
-
---
+++ unirest.js
@@ -0,0 +1,25 @@
+const unirest = require("unirest");
+
+const req = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/");
+const req2 = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/anytime");
+
+const browseDates = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/SFO-sky/LAX-sky/anytime");
+
+
+req.query({
+	"query": "Stockholm",
+	"inboundpartialdate": "anytime"
+});
+
+req.headers({
+	"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+	"x-rapidapi-key": "e7dc0e5c66msha7c62a4525b23dfp1beafdjsnd67679c4edc0"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
