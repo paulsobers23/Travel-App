505d46ea3f7dbd2bbad208d17763dae3504833b8
Anne Hyacinthe <annehyacinthe@Annes-MacBook-Air.local>
2020-02-28 10:12:56 -0500

    n

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const getData = async() =>{
+// const getData = async() =>{
   
-  const data = await fetch("https://cors-anywhere.herokuapp.com//https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01?inboundpartialdate=2019-12-01", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-		"x-rapidapi-key": "e7dc0e5c66msha7c62a4525b23dfp1beafdjsnd67679c4edc0"
-	}
-})
-  .catch(err => {
-	console.log(err);
-});
-  console.log(data)
-  return data
+//   const data = await fetch("https://cors-anywhere.herokuapp.com//https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01?inboundpartialdate=2019-12-01", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+// 		"x-rapidapi-key": "e7dc0e5c66msha7c62a4525b23dfp1beafdjsnd67679c4edc0"
+// 	}
+// })
+//   .catch(err => {
+// 	console.log(err);
+// });
+//   console.log(data)
+//   return data
   
-}
+// }
 
-getData()
+// getData()
+
+const getLocations = async()=>{
+	await fetch("http://partners.api.skyscanner.net/apiservices/geo/v1.0?apiKey=e7dc0e5c66msha7c62a4525b23dfp1beafdjsnd67679c4edc0")
+		.then(response => response.json)
+		.then(console.log(response))
+		.catch(err => console.log(err))
+}
 
 // fetch("https://cors-anywhere.herokuapp.com//https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01?inboundpartialdate=2019-12-01", {
 // 	"method": "GET",
