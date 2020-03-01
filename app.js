8425c274ab2c65bb0a5d781596cf2e12b0cad715
EC2 Default User <ec2-user@ip-172-31-40-240.us-east-2.compute.internal>
2020-03-01 16:25:41 +0000

    added new elements to form , need to work on locating hotels by city based on user input

--- app.js
+++ app.js
@@ -8,105 +8,123 @@ const requestMethod = (method, url, data) => fetch(url, {
   },
 })
 		  .catch((response) => {
-		    const error = new Error('Something went wrong')
-		    error.data = response
-		    console.log(error)
-		    throw error
+		    const error = new Error('Something went wrong');
+		    error.data = response;
+		    console.log(error);
+		    throw error;
 		  });
 
+
+
 // Responsible for locating hotels by the location id and list the name and prices
-const getHotelsByCity = async (locationID) =>{
-  const hotelAPI = `https://tripadvisor1.p.rapidapi.com/hotels/list?location_id=${locationID}&adults=1&rooms=1`
+const getHotelsByCity = async (locationID) => {
+  const hotelAPI = `https://tripadvisor1.p.rapidapi.com/hotels/list?location_id=${locationID}&adults=1&rooms=1`;
   const data = await requestMethod('GET', hotelAPI);
   const response = await data.json();
   console.log(response);
-  const results = response.data.map((hotels)=>{
-    const hotelName = hotels.name
-    const hotelPrice = hotels.price
-    return `Hotel Name: ${hotelName}\n Hotel Price: ${hotelPrice}\n`
-  })
-  console.log(results)
-  return results
-  
-}
-getHotelsByCity('60763')
-
-// Responsible for making sure user submit a certain amount of money and load the next page
-const indexForm = document.getElementById('myForm');
-indexForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const input = document.getElementById('budget');
-  const budget = Number(input.value);
-
-  if (isNaN(budget)) {
-    window.alert('Please enter a number')
-  }
-  if (budget < 500) {
-    document.body.innerHTML += `
-        <p> We apologize but the lowest amount of money to use this app is $500</p>
-    `;
-    location.reload(true)
-  } else{
-    // window.location = 'hotels.html'
-  }
-});
-
+  const results = response.data.map((hotels) => {
+    const hotelName = hotels.name;
+    const hotelPrice = hotels.price;
+    return `Hotel Name: ${hotelName}\n Hotel Price: ${hotelPrice}\n`;
+  });
+  console.log(results);
+  return results;
+};
+// getHotelsByCity('60763')
 
 // List of location ids for the hotels in every state
-let tripAdvisorHotelID = [ 'New York - 60763 ',
-              'Alaska - 28923',
-              'Arkansas - 28925',
-              'Arizona - 31352',
-              'American Samoa - 294490',
-              'Alabama - 28922',
-              'California - 28926 ',
-              'Colorado - 28927',
-              'Connecticut - 28928 ',
-              'District of Columbia - 28969',
-              'Delaware - 28929',
-              'Florida - 28930',
-              'Georgia - 28931',
-              'Guam - 60668',
-              'Hawaii - 28932',
-              'Iowa - 28936',
-              'Idaho - 28933',
-              'Illinois- 28934',
-              'Indiana- 28935',
-              'Kansas - 28937',
-              'Kentucky - 28938',
-              'Louisiana - 28939',
-              'Massachusetts - 28942',
-              'Maryland - 28941',
-              'Maine - 28940',
-              'Michigan - 28943',
-              'Minnesota - 28944',
-              'Missouri - 28946',
-              'Mississippi - 28945',
-              'Montana - 28947',
-              'North Carolina - 1438847',
-              'North Dakota - 28955',
-              'Nebraska - 28948',
-              'New Hampshire - 186297',
-              'New Jersey - 28951',
-              'New Mexico - 28952',
-              'Nevada - 28949',
-              'Ohio - 28956',
-              'Oklahoma - 28957',
-              'Oregon - 28958',
-              'Pennsylvania - 28959',
-              'Puerto Rico - 147319',
-              'Rhode Island - 28960',
-              'South Carolina - 659476',
-              'South Dakota - 28962',
-              'Tennessee - 28963',
-              'Texas - 28964 ',
-              'Utah - 28965',
-              'Virginia - 28967 ',
-              'Virgin Islands - 147400',
-              'Vermont - 28966',
-              'Washington - 28968',
-              'isconsin - 28972',
-              'West Virginia - 28971',
-              'Wyoming - 28973'
-              
-]
+const tripAdvisorHotelID = {
+  'New York': '60763 ',
+  'Alaska': '28923',
+  'Arkansas': ' 28925',
+  'Arizona': '31352',
+  'American Samoa': '294490',
+  'Alabama': '28922',
+  'California': '28926 ',
+  'Colorado': ' 28927',
+  'Connecticut': '28928',
+  'District of Columbia': '28969',
+  'Delaware': ' 28929',
+  'Florida': ' 28930',
+  'Georgia': ' 28931',
+  'Guam': '60668',
+  'Hawaii': '28932',
+  'Iowa': ' 28936',
+  'Idaho': ' 28933',
+  'Illinois': '28934',
+  'Indiana': ' 28935',
+  'Kansas': ' 28937',
+  'Kentucky': ' 28938',
+  'Louisiana': ' 28939',
+  'Massachusetts': '28942',
+  'Maryland': ' 28941',
+  'Maine': ' 28940',
+  'Michigan': '28943',
+  'Minnesota': ' 28944',
+  'Missouri': ' 28946',
+  'Mississippi': ' 28945',
+  'Montana': ' 28947',
+  'North Carolina': ' 1438847',
+  'North Dakota': ' 28955',
+  'Nebraska': ' 28948',
+  'New Hampshire': ' 186297',
+  'New Jersey': ' 28951',
+  'New Mexico': ' 28952',
+  'Nevada': ' 28949',
+  'Ohio': ' 28956',
+  'Oklahoma': ' 28957',
+  'Oregon': ' 28958',
+  'Pennsylvania': ' 28959',
+  'Puerto Rico': '147319',
+  'Rhode Island': ' 28960',
+  'South Carolina': ' 659476',
+  'South Dakota': ' 28962',
+  'Tennessee': ' 28963',
+  'Texas': ' 28964 ',
+  'Utah': ' 28965',
+  'Virginia': '28967 ',
+  'Virgin Islands': ' 147400',
+  'Vermont': ' 28966',
+  'Washington ': ' 28968',
+  'Wisconsin': ' 28972',
+  'West Virginia': ' 28971',
+  'Wyoming': ' 28973',
+};
+
+const getHotelsID = (obj) =>{
+  for(let city in obj){
+    // console.log(city)
+    // return city
+  }
+}
+getHotelsID(tripAdvisorHotelID);
+
+
+
+
+// Responsible for making sure user submit a certain amount of money on index.html and load the next page
+const indexForm = document.getElementById('myForm');
+const section = document.getElementById('display');
+indexForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input1 = document.getElementById('budget');
+  const budget = Number(input1.value);
+
+  const city = document.getElementById('city');
+  const arrival = document.getElementById('arrival');
+  const departure = document.getElementById('departure');
+  console.log(budget);
+  console.log(city.value);
+  console.log(departure.value);
+  console.log(arrival.value);
+
+  if (budget >= 500) {
+
+  }
+  if (Number.isNaN(budget)) {
+    // window.alert('Please enter a number')
+  }
+  if (budget < 500) {
+    // window.alert('We apologize but to use this app correctly please enter $500 or greater')
+  }
+});
